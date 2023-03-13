46ac685754b83f16ad05dbd1a8f8d7c979fa9dc4
Husnain Qadri <husnainqadri791@gmail.com>
2023-03-13 15:27:41 +0500

    commit

--- app.py
+++ app.py
@@ -8,27 +8,20 @@ app = Flask(_name_)
 # define the route for the prediction API
 @app.route('/predict', methods=['POST'])
 def predict():
-    # load the trained model from the pickle file
+    
     with open('linear_regression_model.pkl', 'rb') as file:
         model = pickle.load(file)
 
-    # get the input data from the request
     data = request.get_json()
-
-    # convert the input data into a numpy array
+    
     X = np.array([[data['yearsExperience']]])
 
-    # make a prediction using the model
     prediction = model.predict(X)
-
-    # create a response object
     response = {
         'prediction': prediction[0]
     }
 
-    # return the response as a JSON object
     return jsonify(response)
 
-# start the Flask app
 if _name_ == '_main_':
     app.run(debug=True)
